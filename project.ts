3760bd13bfc8f38a48ab8c3323f8619a3653b31e
hiroyuki <hiroyuki@iMac2017.local>
2018-02-11 18:21:43 +0900

    JavaScriptの特徴を利用し中身をシンプルに

--- project.ts
+++ project.ts
@@ -14,65 +14,11 @@ export class Project {
      *       this.name = name;
      *       ...
      *   }
-     * @param _name プロジェクト名
-     * @param _groupId プロジェクトが属するグループまたはサブグループID
-     * @param _description プロジェクトの説明
-     */
-    constructor(private _name: string, private _groupId: string, private _description: string) {
-    }
-
-    /**
-     * プロジェクト名を設定します。
      * @param name プロジェクト名
+     * @param id プロジェクトが属するグループまたはサブグループID
+     * @param des プロジェクトの説明
      */
-    set name(name: string) {
-        this._name = name;
+    constructor(public name: string, public id: string, public desc: string) {
     }
 
-    /**
-     * プロジェクト名を取得します。
-     * @return プロジェクト名
-     */
-    get name(): string {
-        return this._name;
-    }
-
-    /**
-     * プロジェクトが属するグループまたはサブグループIDを設定します。
-     * @param groupId プロジェクトが属するグループまたはサブグループID
-     */
-    set groupId(groupId: string) {
-        this._groupId = groupId;
-    }
-
-    /**
-     * プロジェクトが属するグループまたはサブグループIDを取得します。
-     * @return プロジェクトが属するグループまたはサブグループID
-     */
-    get groupId(): string {
-        return this._groupId;
-    }
-
-    /**
-     * プロジェクトの説明を設定します。
-     * @param description プロジェクトの説明
-     */
-    set description(description: string) {
-        this._description = description;
-    }
-
-    /**
-     * プロジェクトの説明を取得します。
-     * @return プロジェクトの説明
-     */
-    get description(): string {
-        return this._description;
-    }
-
-    /**
-     * このオブジェクトをJSONオブジェクトで返却します。
-     */
-    toJson(): any {
-        return { "name": this._name, "id": this._groupId, "desc": this._description };
-    }
 }
